f28e929b1eb1c54596a3e68c28625d244abcde94
tangbingk <731685340@qq.com>
2020-07-14 15:10:00 +0800

    Initial Commit

---
+++ HealthCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HealthCode
+//
+//  Created by New on 2020/7/14.
+//  Copyright © 2020 Godlowd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
